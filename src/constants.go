8cc9d3804443a9197ef4f57f699598f1b5e1b627
nicolaasuni <info@tecnick.com>
2015-11-26 17:36:22 +0000

    status badges on README

--- src/constants.go
+++ src/constants.go
@@ -5,7 +5,7 @@ const AppName = "rndpwd"
 
 // AppVersion contains this application version
 // This is supposed to be automatically populated by the Makefile using the value from the VERSION file
-const AppVersion = "1.1.0"
+const AppVersion = "1.1.4"
 
 // ValidCharset is a string containing the valid characters for a password
 const ValidCharset = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
